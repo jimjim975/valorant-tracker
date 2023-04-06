36caa2aca0c5ed4f08d93a92a4fa7ce386ca149c
Jason Dufresne <jason.alex.d@protonmail.com>
2023-04-05 22:33:39 -0400

    basic form

---
+++ valorant-tracker/src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Home() {
+  return (
+    <form>
+      <label>Username
+        <input type="text" />
+      </label>
+      <label>Password
+        <input type="text" />
+      </label>
+    </form>
+  )
+}
+
+export default Home;
